3b659400d1a1b9160835f02ed64a868c124bdd1c
Kamsy112 <iykembah1975@gmail.com>
2024-08-27 07:59:58 +0100

    added additional codes

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 function myFunction(){
-    document.getElementById("p1").innerHTML
-    document.getElementById("DOM").innerHTML
-    document.getElementById("p2").innerHTML
-    document.getElementById("DOM").innerHTML
+    document.getElementById("p1").innerHTML=
+    document.getElementById("DOM").firstChild.nodeValue;
+    document.getElementById("p2").innerHTML=
+    document.getElementById("DOM").childNodes[0].nodeValue;
 }
