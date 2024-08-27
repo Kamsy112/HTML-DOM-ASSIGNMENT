9afbe7f2da5dc06fc99d0aa4809e18ad89bc2b52
Kamsy112 <iykembah1975@gmail.com>
2024-08-27 07:04:45 +0100

    iniyial

---
+++ script.js
@@ -0,0 +1,6 @@
+function myFunction(){
+    document.getElementById("p1").innerHTML
+    document.getElementById("DOM").innerHTML
+    document.getElementById("p2").innerHTML
+    document.getElementById("DOM").innerHTML
+}
